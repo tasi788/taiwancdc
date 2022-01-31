345febcb361ccddf452b283199b3aa41a1759cb8
tasi788 <tdc@tdccc.com.tw>
2022-01-31 20:14:36 +0800

    ✨ port 給 【停班停課最新通知｜討論區】

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import {parseFeed} from 'htmlparser2';
 
 const url = "https://www.cdc.gov.tw/RSS/RssXml/Hh094B49-DRwe2RR4eFfrQ?type=1"
 const tg = "https://api.telegram.org/bot" + tgtoken + "/sendMessage"
+const notifys = [-1001224810715, -1001452847515]
 
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
@@ -45,51 +46,55 @@ async function handleRequest(request) {
     for (const entry of entries.items) {
         let value = await taiwancdc.get(entry.link)
         let firstRun = await taiwancdc.get("first")
-        if (value === null && firstRun === "true") {
-            let post = "🗞 " + entry.title + "\n\n"
-            post += entry.description.replace(/<\/?[^>]+(>|$)/g, "");
-            post += "\n\n"
-            post += "📅 " + entry.id
-
-            const keyboard = {
-                inline_keyboard: [[
-                    {
-                        text: "🔗 原文網址",
-                        url: entry.link
+        if (firstRun === "true") {
+            taiwancdc.put(entry.link, 1, {expirationTtl: 604800})
+            return new Response(
+                JSON.stringify({
+                    status: "true",
+                    text: "setted firstRun"
+                }, null, 2), {
+                    headers: {
+                        "content-type": "application/json"
                     }
-                ]]
-            }
-            let payload = {
-                chat_id: -1001224810715,
-                text: post,
-                reply_markup: keyboard
-            }
-            const init = {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(payload)
-            }
-            if (firstRun === "true") {
-                taiwancdc.put(entry.link, 1, {expirationTtl: 604800})
-                return new Response(
-                    JSON.stringify({
-                        status: "true",
-                        text: "setted firstRun"
-                    }, null, 2), {
-                        headers: {
-                            "content-type": "application/json"
+                })
+        } else {
+            if (value === null) {
+                let post = "🗞 " + entry.title + "\n\n"
+                post += entry.description.replace(/<\/?[^>]+(>|$)/g, "");
+                post += "\n\n"
+                post += "📅 " + entry.id
+
+                const keyboard = {
+                    inline_keyboard: [[
+                        {
+                            text: "🔗 原文網址",
+                            url: entry.link
                         }
-                    })
-            } else {
-                let resp = await fetch(tg, init)
-                // let result = await gatherResponse(resp)
-                if (resp.status === 200) {
-                    taiwancdc.put(entry.link, 1, {expirationTtl: 604800})
+                    ]]
+                }
+                for (const notify of notifys) {
+                    let payload = {
+                        chat_id: notify,
+                        text: post,
+                        reply_markup: keyboard
+                    }
+                    const init = {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(payload)
+                    }
+                    let resp = await fetch(tg, init)
+                    // let result = await gatherResponse(resp)
+                    if (resp.status === 200) {
+                        taiwancdc.put(entry.link, 1, {expirationTtl: 604800})
+                    }
                 }
             }
         }
+
+
     }
     return new Response(
         JSON.stringify({
